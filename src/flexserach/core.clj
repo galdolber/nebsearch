d4db341b0ff105f4ac3e846bbba94c9e6752f5ac
Fabricio Cozzarolo <fabriciocozzarolo@gmail.com>
2020-07-31 15:21:08 -0300

    primer funcion agregada y su test

--- src/flexserach/core.clj
+++ src/flexserach/core.clj
@@ -1,22 +1,46 @@
 (ns flexserach.core)
 
+(def defaults
+  {:encode "icase"
+   :tokenize "forward"
+   :split #"\W+"
+   :cache false
+   :async false
+   :worker false
+   :rtl false
+   :resolution 9
+   :threshold 0
+   :depth 0})
+
+(def presets
+  {:memory {:encode "extra" :tokenize "strict" :threshold 0 :resolution 1}
+   :speed {:encode "icase" :tokenize "strict" :threshold 1 :resolution 3 :depth 2}
+   :match  {:encode "extra" :tokenize "full" :threshold 1 :resolution 3}
+   :score {:encode "extra" :tokenize "strict" :threshold 1 :resolution 9 :depth 4}
+   :balance {:encode "balance" :tokenize "strict" :threshold 0 :resolution 3 :depth 3}
+   :fast {:encode "icase" :tokenize "strict" :threshold 8 :resolution 9 :depth 1}})
+
+(defn sort-by-length-down [a b]
+  (let [diff (- (count a) (count b))]
+    (if (< diff 0) 1 (if diff (- 1) 0))))
+
 (def char-prev-is-vowel #{\a \e \i \o \u \y})
 
 (defn collapse-repeating-chars [string]
-  (loop [collapsed_string ""
-         char_prev nil
+  (loop [collapsed-string ""
+         char-prev nil
          [[i char] & cx] (map-indexed vector string)]
     (if char
-      (let [char_next (first cx)]
-        (recur (if (not= char char_prev)
+      (let [char-next (first cx)]
+        (recur (if (not= char char-prev)
                  (if (and (pos? i) (= \h char))
-                   (if (or (and (char-prev-is-vowel char_prev)
-                                (char-prev-is-vowel char_next))
-                           (= char_prev \ ))
-                     (str collapsed_string char)
-                     collapsed_string)
-                   (str collapsed_string char))
-                 collapsed_string)
+                   (if (or (and (char-prev-is-vowel char-prev)
+                                (char-prev-is-vowel char-next))
+                           (= char-prev \ ))
+                     (str collapsed-string char)
+                     collapsed-string)
+                   (str collapsed-string char))
+                 collapsed-string)
                char
                cx))
-      collapsed_string)))
+      collapsed-string)))
--- test/flexserach/core_test.clj
+++ test/flexserach/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer [deftest is]]
             [flexserach.core :as f]))
 
+(deftest sort-by-length-down-test
+  (is (= (- 1) (f/sort-by-length-down "abc" "ab")))
+  (is (= (- 1) (f/sort-by-length-down "abc" "abc")))
+  (is (= 1 (f/sort-by-length-down "abc" "abcd"))))
+
 (deftest collapse-repeating-chars-test
   (is (= (f/collapse-repeating-chars "hello") "helo"))
   (is (= (f/collapse-repeating-chars "ihello") "ielo")))
