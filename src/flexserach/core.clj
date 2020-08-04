5b851e96a5c34cb5d5c01c319095c625c10e8bd8
Fabricio Cozzarolo <fabriciocozzarolo@gmail.com>
2020-08-04 02:35:07 -0300

    Hasta global-encoder
    veo que usas #js para crear arrays... y aget, aset y set!....eso lo
    tengo que mantener asi o puedo pasar todo a vectores?

--- src/flexserach/core.clj
+++ src/flexserach/core.clj
@@ -1,6 +1,8 @@
 (ns flexserach.core
   (:require [clojure.string :as str]))
 
+;;AGREGAR LOS WHEN CUANDO HAGAN FALTA Y SUS TESTS
+
 (def defaults
   {:encode "icase"
    :tokenize "forward"
@@ -93,6 +95,11 @@
    [#"[vw]" "f"]
    [#"[aeiouy]" ""]])
 
+(def balance-regex
+  [[#"[-/]" " "]
+   [#"[^a-z0-9 ]" ""]
+   [#"\s+" " "]])
+
 (defn replace-regexes [str regexp]
   (reduce (fn [str [regex rep]]
             (str/replace str regex rep))
@@ -119,4 +126,60 @@
                          string)
             :else string))))
 
-;;;;ignore anda bien?
+(defn global-encoder-extra [string]
+  (if-not string
+    string
+    (let [string (global-encoder-advanced string true)]
+      (cond (< 1 (count string))
+            (collapse-repeating-chars
+             (str/join " "
+                       (loop [string (str/split string #" ")
+                              current (get string 0)
+                              c 0]
+                         (println string current c)
+                         (cond (= c (count string)) string
+                               :else (recur (if (< 1 (count current))
+                                              (assoc string c
+                                                     (str (first current)
+                                                          (replace-regexes
+                                                           (subs current 1)
+                                                           extra-regex)))
+                                              string)
+                                            (get string (inc c))
+                                            (inc c))))))
+            :else string))))
+
+(defn global-encoder-balance [string]
+  (when string
+    (collapse-repeating-chars (replace-regexes (str/lower-case string) balance-regex))))
+
+(def global-encoder
+  {:icase global-encoder-icase
+   :simple global-encoder-simple
+   :advanced global-encoder-advanced
+   :extra global-encoder-extra
+   :balance global-encoder-balance})
+
+(defn create-page [cursor page result];;FALTAN LOS TESTS, NO SE LOS INPUTS
+  ;; TODO str page?
+  (if cursor
+    {:page cursor, :next (if page (str page) nil), :result result}
+    result))
+
+(defn add-index [map dupes value id partial-score context-score threshold resolution];;FALTAN LOS TESTS, NO SE LOS INPUTS
+  (when (aget dupes value) (aget dupes value))
+  (let [score (if partial-score
+                (+
+                 (* (- resolution (or threshold (/ resolution 1.5))) context-score)
+                 (* (or threshold (/ resolution 1.5)) partial-score))
+                context-score)]
+    (aset dupes value score)
+    (when (>= score threshold)
+      ;; TODO?
+      (let [arr (aget map (- resolution (bit-shift-right #_>> (+ score 0.5) 0)))];;LE DEJAS EL SHIFT COMENTADO POR ALGO?
+        (set! arr (or (aget arr value) (aset arr value #js [])))
+        (aset arr (.-length arr) id)))
+    score))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
