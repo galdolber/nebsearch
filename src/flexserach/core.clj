e5ec921c905a1a082e6605c3bcafa9ab81e79a86
Gal Dolber <gal.dolber@gmail.com>
2020-09-23 03:47:41 -0300

    Remove all fat

--- src/flexserach/core.clj
+++ src/flexserach/core.clj
@@ -2,142 +2,39 @@
   (:require [clojure.string :as string]
             [clojure.set :as sets]))
 
-(def char-prev-is-vowel #{\a \e \i \o \u \y})
-
 (defn collapse-repeating-chars [string]
-  (loop [collapsed-string ""
-         char-prev nil
-         [[i char] & cx] (map-indexed vector string)]
-    (if char
-      (let [char-next (first cx)]
-        (recur (if (not= char char-prev)
-                 (if (and (pos? i) (= \h char))
-                   (if (or (and (char-prev-is-vowel char-prev)
-                                (char-prev-is-vowel char-next))
-                           (= char-prev \ ))
-                     (str collapsed-string char)
-                     collapsed-string)
-                   (str collapsed-string char))
-                 collapsed-string)
-               char
-               cx))
-      collapsed-string)))
+  (string/replace string #"(.)(?=.*\1)" ""))
 
-(def simple-regex
-  [[#"[àáâãäå]" "a"]
-   [#"[èéêë]" "e"]
-   [#"[ìíîï]" "i"]
-   [#"[òóôõöő]" "o"]
-   [#"[ùúûüű]" "u"]
-   [#"[ýŷÿ]" "y"]
-   [#"ñ" "n"]
-   [#"[çc]" "k"]
-   [#"ß" "s"]
-   [#" & " " and "]
-   [#"[-/]" " "]
-   [#"[^a-z0-9 ]" ""] ;; there's a space at the end
-   [#"\s+" " "]])
+(defn ^String normalize [^String str]
+  (let [^String normalized (java.text.Normalizer/normalize str java.text.Normalizer$Form/NFD)]
+    (clojure.string/replace normalized #"\p{InCombiningDiacriticalMarks}+" "")))
 
-(def advanced-regex
-  [[#"ae" "a"]
-   [#"ai" "ei"]
-   [#"ay" "ei"]
-   [#"ey" "ei"]
-   [#"oe" "o"]
-   [#"ue" "u"]
-   [#"ie" "i"]
-   [#"sz" "s"]
-   [#"zs" "s"]
-   [#"sh" "s"]
-   [#"ck" "k"]
-   [#"cc" "k"]
-   [#"th" "t"]
-   [#"dt" "t"]
-   [#"ph" "f"]
-   [#"pf" "f"]
-   [#"ou" "o"]
-   [#"uo" "u"]])
+#_(defn ^string normalize-cljs [^string s] (.replace (.normalize s "NFD") #"[\u0300-\u036f]" ""))
 
-(def extra-regex
-  [[#"p" "b"]
-   [#"z" "s"]
-   [#"[cgq]" "k"]
-   [#"n" "m"]
-   [#"d" "t"]
-   [#"[vw]" "f"]
-   [#"[aeiouy]" ""]])
-
-(def balance-regex
-  [[#"[-/]" " "]
-   [#"[^a-z0-9 ]" ""]
-   [#"\s+" " "]])
-
-(defn replace-regexes [str regexp]
-  (if (seq regexp)
-    (reduce (fn [str [regex rep]]
-              (string/replace str regex rep))
-            str
-            regexp)
-    str))
+(defn replace-regexes [^String str regexp]
+  (reduce (fn [^String str [regex rep]]
+            (string/replace str regex rep))
+          str
+          regexp))
 
 (defn encoder-icase [value]
   (string/lower-case value))
 
 (defn encoder-simple [value]
   (when value
-    (let [s (replace-regexes (string/lower-case value) simple-regex)]
+    (let [s (normalize (string/lower-case value))]
       (if (string/blank? s) "" s))))
 
-;; This one is slow!! optimize
-(defn encoder-advanced
-  ([string] (encoder-advanced string false))
-  ([string skip]
-   (if-not string
-     string
-     (let [string (encoder-simple string)]
-       (if (< 2 (count string))
-         (if (and (not skip) (< 1 (count string)))
-           (collapse-repeating-chars (replace-regexes string advanced-regex))
-           (replace-regexes string advanced-regex))
-         (if (not skip)
-           (if (< 1 (count string))
-             (collapse-repeating-chars string)
-             string)
-           string))))))
-
-(defn encoder-extra [string]
-  (if-not string
-    string
-    (let [string (encoder-advanced string true)]
-      (if (< 1 (count string))
-        (collapse-repeating-chars
-         (string/join " "
-                      (loop [string (string/split string #" ")
-                             current (get string 0)
-                             c 0]
-                        (if (= c (count string)) string
-                            (recur (if (< 1 (count current))
-                                     (assoc string c
-                                            (str (first current)
-                                                 (replace-regexes
-                                                  (subs current 1)
-                                                  extra-regex)))
-                                     string)
-                                   (get string (inc c))
-                                   (inc c))))))
-        string))))
-
-(defn encoder-balance [string]
+(defn encoder-advanced [string]
   (when string
-    (collapse-repeating-chars (replace-regexes (string/lower-case string) balance-regex))))
+    (let [string (encoder-simple string)]
+      (collapse-repeating-chars string))))
 
 (defn get-encoder [encoder]
   (case encoder
     :icase encoder-icase
     :simple encoder-simple
     :advanced encoder-advanced
-    :extra encoder-extra
-    :balance encoder-balance
     encoder-icase))
 
 (defn encode-value [{:keys [encoder stemmer matcher]} value]
@@ -159,34 +56,33 @@
         (update :filter #(when % (set (mapv encoder %)))))))
 
 (defn add-index [data value id]
-  (update-in data (conj (vec (seq value)) :<) #(conj (or % #{}) id)) )
+  ;;(update-in data (conj (vec (seq value)) :<) #(conj (or % #{}) id))
+  (update data value #(conj (or % #{}) id)))
 
 (defn remove-index [data value id]
-  (update-in data (conj (vec (seq value)) :<) #(disj (or % #{}) id)))
+  #_(update-in data (conj (vec (seq value)) :<) #(disj (or % #{}) id))
+  (update data value #(disj (or % #{}) id)))
 
 (defn index-reverse [data operation value id]
-  (let [value (vec value)]
-    (reduce (fn [data n]
-              (operation data (drop n value) id)) data (range (count value)))))
+  (reduce (fn [data n]
+            (operation data (subs value n) id)) data (range (count value))))
 
 (defn index-forward [data operation value id]
-  (let [value (vec value)]
-    (reduce (fn [data n]
-              (operation data (take n value) id)) data (range (count value)))))
+  (reduce (fn [data n]
+            (operation data (subs value 0 n) id)) data (range (count value))))
 
 (defn index-both [data operation value id]
   (index-forward (index-reverse data operation value id) operation value id))
 
 (defn index-full [data operation value id]
-  (let [value (vec value)]
-    (reduce
-     (fn [data from]
-       (let [value (drop from value)]
-         (reduce (fn [data n]
-                   (operation data (take n value) id)) data
-                 (range (inc (count value))))))
-     data
-     (range (dec (count value))))))
+  (reduce
+   (fn [data from]
+     (let [value (subs value from)]
+       (reduce (fn [data n]
+                 (operation data (subs value 0 n) id)) data
+               (range (inc (count value))))))
+   data
+   (range (dec (count value)))))
 
 (defn get-indexer [indexing]
   (case indexing
@@ -236,7 +132,7 @@
           words (tokenizer search)
           words (set (if filter (filter-words words filter) words))]
       ;; TODO? add threshold?
-      (reduce into #{} (mapv (fn [word] (:< (get-in data (seq word)))) words)))))
+      (reduce into #{} (mapv data words)))))
 
 (comment
   (let [flex (-> (init {:indexer :full :filter #{"el"}})
