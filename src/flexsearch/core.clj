64d70edb495d3c37a990bb5970fdc14948be8913
Gal Dolber <gal.dolber@gmail.com>
2020-09-28 04:18:52 -0300

    First commit

--- src/flexsearch/core.clj
+++ src/flexsearch/core.clj
@@ -51,26 +51,42 @@
 (defn filter-words [words filterer]
   (vec (remove filterer words)))
 
-(defn index-reverse [data operation ^String value id]
-  (reduce (fn [data n]
-            (operation data (subs value n) id)) data (range (count value))))
+(defn index-reverse [^String value]
+  (let [len (count value)]
+    (loop [i 0
+           r (transient #{})]
+      (if (= len i)
+        (persistent! r)
+        (recur (inc i) (conj! r (.substring value i)))))))
 
-(defn index-forward [data operation ^String value id]
-  (reduce (fn [data n]
-            (operation data (subs value 0 n) id)) data (range 1 (inc (count value)))))
+(defn index-forward [^String value]
+  (let [len (inc (count value))]
+    (loop [i 1
+           r (transient [])]
+      (if (= len i)
+        (persistent! r)
+        (recur (inc i) (conj! r (.substring value 0 i)))))))
 
-(defn index-both [data operation ^String value id]
-  (index-forward (index-reverse data operation value id) operation value id))
+(defn index-both [^String value]
+  (let [len (inc (count value))]
+    (loop [i 1
+           r (transient [])]
+      (if (= len i)
+        (persistent! r)
+        (recur (inc i) (conj! (conj! r (.substring value 0 i)) (.substring value (dec i))))))))
 
-(defn index-full [data operation ^String value id]
-  (reduce
-   (fn [data from]
-     (let [value (subs value from)]
-       (reduce (fn [data n]
-                 (operation data (subs value 0 n) id)) data
-               (range 1 (inc (count value))))))
-   data
-   (range (dec (count value)))))
+(defn index-full [^String value]
+  (let [len (count value)
+        llen (dec len)]
+    (loop [i 0
+           j 1
+           r (transient [])]
+      (if (and (= llen i) (= len j))
+        (persistent! r)
+        (let [r (conj! r (.substring value i j))]
+          (if (= len j)
+            (recur (inc i) (+ i 2) r)
+            (recur i (inc j) r)))))))
 
 (defn get-indexer [indexing]
   (case indexing
@@ -81,7 +97,7 @@
     index-forward))
 
 (defn default-splitter [^String s]
-  (string/split s #"[\W+|[^A-Za-z0-9]]"))
+  (remove string/blank? (string/split s #"[\W+|[^A-Za-z0-9]]")))
 
 (defn init [{:keys [tokenizer indexer filter] :as options}]
   (let [encoder (get-encoder (:encoder options))]
@@ -91,55 +107,29 @@
            :tokenizer (if (fn? tokenizer) tokenizer default-splitter)
            :filter (set (mapv encoder filter)))))
 
-(defn add-index [data ^String value id]
-  #_(assoc data value (conj (or (get data value) #{}) id))
-  (update data value #(conj (or % #{}) id)))
-
-(defn remove-index [data ^String value id]
-  #_(assoc data value (disj (or (get data value) #{}) id))
-  (update data value #(disj (or % #{}) id)))
-
-(defn remove-indexes [flex indexer words id]
-  (assoc flex :data
-         (reduce (fn [data word]
-                   (indexer data remove-index word id)) (:data flex) words)))
-
-(defn add-indexes [flex indexer words id]
-  (assoc flex :data
-         (reduce (fn [data word]
-                   (indexer data add-index word id)) (:data flex) words)))
-
 (defn flex-add
-  [{:keys [ids tokenizer indexer filter] :as flex} id content]
+  [{:keys [tokenizer indexer filter] :as flex} id content]
   (let [content (encode-value flex content)
         words (tokenizer content)
-        words (set (if filter (filter-words words filter) words))]
-    (if-let [old-words (get ids id)]
-      (let [added (sets/difference words old-words)
-            deleted (sets/difference old-words words)]
-        (-> flex
-            (remove-indexes indexer deleted id)
-            (add-indexes indexer added id)
-            (assoc-in [:ids id] words)))
-      (-> flex
-          (add-indexes indexer words id)
-          (assoc-in [:ids id] words)))))
+        words (set (if filter (filter-words words filter) words))
+        words (set (mapcat indexer words))]
+    (assoc-in flex [:ids id] words)))
 
-(defn flex-remove [{:keys [ids indexer] :as flex} id]
-  (if-let [old-words (get ids id)]
-    (-> flex
-        (remove-indexes indexer old-words id)
-        (update :ids #(dissoc % id)))
-    flex))
+(defn flex-remove [flex id]
+  (update flex :ids #(dissoc % id)))
 
-(defn flex-search [{:keys [data tokenizer filter] :as flex} search]
-  (when (and search data)
+(defn flex-search [{:keys [ids tokenizer] :as flex} search]
+  (when (and search ids)
     (let [search (encode-value flex search)
           words (tokenizer search)
-          words (set (if filter (filter-words words filter) words))]
+          words (set (if-let [f (:filter flex)] (filter-words words f) words))]
       ;; TODO? add threshold?
       #_(reduce into #{} (mapv data words))
-      (apply sets/intersection (mapv data words)))))
+      (set
+       (mapv first
+             (filter (fn [[_ tokens]]
+                       (sets/superset? tokens words)) ids)))
+      #_(apply sets/intersection (mapv data words)))))
 
 (def sample-data (read-string (slurp "data.edn")))
 
@@ -147,5 +137,5 @@
   (println (read-line))
   (time (let [flex (init {:indexer :full :encoder :advanced})
               flex (reduce (fn [flex [k v]]
-                             (flex-add flex k v)) flex (map vector (range) sample-data))]
-          (flex-search flex "and jus"))))
+                             (flex-add flex k v)) flex (map vector sample-data #_(range) sample-data))]
+        (time (flex-search flex "midnig boston")))))
