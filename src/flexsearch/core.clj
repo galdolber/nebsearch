fa4a01211e1e6bdce4b160503061fe5b2d2cb2e1
Fabricio Cozzarolo <fabriciocozzarolo@gmail.com>
2020-09-29 15:20:52 -0300

    Flex-remove

--- src/flexsearch/core.clj
+++ src/flexsearch/core.clj
@@ -40,7 +40,8 @@
   (vec (remove filterer words)))
 
 (defn default-splitter [^String s]
-  (string/split s #"[\W+|[^A-Za-z0-9]]"))
+  (string/split s #"[\W+]|[^A-Za-z0-9]"))
+
 
 (defn init [{:keys [tokenizer filter] :as options}]
   (let [encoder (get-encoder (:encoder options))]
@@ -48,23 +49,64 @@
            :encoder encoder
            :tokenizer (if (fn? tokenizer) tokenizer default-splitter)
            :filter (set (mapv encoder filter)))))
+(def flex-s (init {:encoder :simple}))
+
 
 ;; TODO support updates
-(defn flex-add [{:keys [index data encoder] :as flex} pairs]
+(defn flex-add [{:keys [index data encoder] :as flex} pairs];;([0 "Rambo"][1 "Mision Imposible"]...)
   (loop [[[id w] & ws] pairs
          pos (count index)
          data data
          r []]
     (if w
       (let [w (encoder w)]
-        (recur ws (+ pos (count w) 1) (conj data [pos id]) (conj r w)))
+        (recur ws
+               (+ pos (count w) 1)
+               (conj data [pos id])
+               (conj r w)))
       (assoc flex
              :index (str index (when-not (string/blank? index) "$") (string/join "$" r))
              :data data))))
 
+(def pelis ["$ aka Dollars"
+            "$1,000 a Touchdown"
+            "$10 Raise"
+            "$10,000 Under a Pillow"
+            "$50,000 Reward"])
+
+(def pares (map vector (range) pelis))
+
+(def entero (flex-add flex-s pares))
+
+
+
+#_{:data #{[0 0] [14 1] [33 2] [43 3] [66 4]}
+ :encoder encoder-simple
+ :tokenizer default-splitter
+ :filter #{}
+ :index "$ aka dollars$$1,000 a touchdown$$10 raise$$10,000 under a pillow$$50,000 reward"}
+
+
+(subs "$ aka dollars$$1,000 a touchdown$$10 raise$$10,000 under a pillow" (+ 14 18))
+
+
 ;; TODO
-(defn flex-remove [{:keys [] :as flex} id]
-  )
+(defn flex-remove [{:keys [data index] :as flex} id]
+  (assoc flex
+         :data (apply pss/sorted-set (remove (fn [[pos x]] (= id x)) data))
+         :index (let [pair (flatten (filter (fn [[pos n]] (= n id)) data))]
+                  (str (subs index 0 (dec (first pair)))
+                       (subs index
+                             (+ (first pair)
+                                (count
+                                 (apply second
+                                        (filter (fn [[x name]] (= x id))
+                                                (map vector
+                                                     (range)
+                                                     (read-string (slurp "data.edn"))))))))))))
+
+
+
 
 (defn find-positions [text search]
   (let [search-len (count search)]
@@ -93,3 +135,29 @@
     (read-line)
     (mapv sample-data (time (flex-search flex "gal lade")))
     (read-line)))
+
+
+(def pares (map vector (range) (read-string (slurp "data.edn"))))
+
+(time (flex-add (init {:encoder :simple})
+                (map vector (range) (read-string (slurp "data.edn")))))
+
+
+(map vector (range) ["$ aka Dollars"
+                     "$1,000 a Touchdown"
+                     "$10 Raise"
+                     "$10,000 Under a Pillow"
+                     "$50,000 Reward"]))
+
+'([0 "$ aka Dollars"]
+  [1 "$1,000 a Touchdown"]
+  [2 "$10 Raise"]
+  [3 "$10,000 Under a Pillow"]
+  [4 "$50,000 Reward"]
+  [5 "(500) Days of Summer"])
+
+{:encoder
+ :tokenizer
+ :filter
+ :data #{[0 0] [14 1] [33 2] [43 3] [66 4]}
+ :index "$ aka dollars$$1,000 a touchdown$$10 raise$"}
