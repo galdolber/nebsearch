43fd39a1d8dc058090b58795bb232612f3208bf3
Fabricio Cozzarolo <fabriciocozzarolo@gmail.com>
2020-09-30 00:04:28 -0300

    flex-remove y flex-update

--- src/flexsearch/core.clj
+++ src/flexsearch/core.clj
@@ -12,7 +12,6 @@
 (defn ^String normalize [^String str]
   (let [^String normalized (java.text.Normalizer/normalize str java.text.Normalizer$Form/NFD)]
     (clojure.string/replace normalized #"\p{InCombiningDiacriticalMarks}+" "")))
-
 #_(defn ^string normalize-cljs [^string s] (.replace (.normalize s "NFD") #"[\u0300-\u036f]" ""))
 
 (defn encoder-icase [value]
@@ -35,6 +34,37 @@
     :advanced encoder-advanced
     encoder-icase))
 
+(def prepositions
+  #{"about" "beside" "near" "to" "above" "between" "of" "towards" "across" "beyond" "off" "under" "after" "by" "on" "underneath" "against" "despite" "onto" "unlike" "along" "down" "opposite" "until" "among" "during" "out" "up" "around" "except" "outside" "upon" "as" "for" "over" "via" "at" "from" "past" "with" "before" "in" "round" "within" "behind" "inside" "since" "without" "below" "into" "than" "beneath" "like" "through" "Since" "Within" "Underneath" "Beside" "Under" "Towards" "Of" "After" "Up" "Off" "Among" "Beyond" "Via" "Over" "By" "Like" "Onto" "About" "Without" "Than" "For" "Past" "Along" "Outside" "Despite" "Upon" "On" "Above" "Opposite" "Until" "Out" "Down" "Between" "Against" "From" "Inside" "Across" "Beneath" "With" "Around" "Through" "Behind" "During" "Unlike" "Before" "To" "Into" "Round" "Except" "As" "At" "Below" "Near" "In" "and" "And" "or" "Or"})
+
+(def advanced-regex
+  [[#"ae" "a"]
+   [#"ai" "ei"]
+   [#"ay" "ei"]
+   [#"ey" "ei"]
+   [#"oe" "o"]
+   [#"ue" "u"]
+   [#"ie" "i"]
+   [#"sz" "s"]
+   [#"zs" "s"]
+   [#"sh" "s"]
+   [#"ck" "k"]
+   [#"cc" "k"]
+   [#"th" "t"]
+   [#"dt" "t"]
+   [#"ph" "f"]
+   [#"pf" "f"]
+   [#"ou" "o"]
+   [#"uo" "u"]])
+
+(defn replace-regexes [^String str regexp]
+  (reduce (fn [^String str [regex rep]]
+            (if (keyword? regex)
+              (.replaceAll str (name regex) rep)
+              (string/replace str regex rep)))
+          str
+          regexp))
+
 ;; TODO new implementation?
 (defn filter-words [words filterer]
   (vec (remove filterer words)))
@@ -49,10 +79,9 @@
            :encoder encoder
            :tokenizer (if (fn? tokenizer) tokenizer default-splitter)
            :filter (set (mapv encoder filter)))))
-(def flex-s (init {:encoder :simple}))
-
 
 ;; TODO support updates
+;; ORIGINAL
 (defn flex-add [{:keys [index data encoder] :as flex} pairs];;([0 "Rambo"][1 "Mision Imposible"]...)
   (loop [[[id w] & ws] pairs
          pos (count index)
@@ -67,53 +96,59 @@
       (assoc flex
              :index (str index (when-not (string/blank? index) "$") (string/join "$" r))
              :data data))))
+;; REGEXES Y FILTER AGREGADOS:
+(defn flex-add [{:keys [index data encoder filter] :as flex} pairs]
+  (loop [[[id w] & ws] pairs
+         pos (count index)
+         data data
+         r []]
+    (if w
+      (let [w (encoder w)]
+        (recur ws
+               (+ pos (count w) 1)
+               (conj data [pos id])
+               (conj r w)))
+      (assoc flex
+             :index (let [r (map (fn [w] (replace-regexes w advanced-regex)) r)
+                          r (if filter (filter-words r filter) r)]
+                         (str index
+                          (when-not (string/blank? index) "$")
+                          (string/join "$" r)))
+             :data data))))
 
-(def pelis ["$ aka Dollars"
-            "$1,000 a Touchdown"
-            "$10 Raise"
-            "$10,000 Under a Pillow"
-            "$50,000 Reward"])
+(defn indexes-of [v]
+  (->> v
+       (map-indexed vector)
+       (clojure.set/map-invert)))
 
-(def pares (map vector (range) pelis))
+;;a lo mejor conviene deja el vector del par en data cuando se remueve como [nil nil].
 
-(def entero (flex-add flex-s pares))
-
-
-
-#_{:data #{[0 0] [14 1] [33 2] [43 3] [66 4]}
- :encoder encoder-simple
- :tokenizer default-splitter
- :filter #{}
- :index "$ aka dollars$$1,000 a touchdown$$10 raise$$10,000 under a pillow$$50,000 reward"}
-
-
-(subs "$ aka dollars$$1,000 a touchdown$$10 raise$$10,000 under a pillow" (+ 14 18))
-
-
-;; TODO
 (defn flex-remove [{:keys [data index] :as flex} id]
-  (assoc flex
-         :data (apply pss/sorted-set (remove (fn [[pos x]] (= id x)) data))
-         :index (let [pair (flatten (filter (fn [[pos n]] (= n id)) data))]
-                  (str (subs index 0 (dec (first pair)))
-                       (subs index
-                             (+ (first pair)
-                                (count
-                                 (apply second
-                                        (filter (fn [[x name]] (= x id))
-                                                (map vector
-                                                     (range)
-                                                     (read-string (slurp "data.edn"))))))))))))
+  (let [pair (flatten (filter (fn [[pos n]] (= n id)) data))
+        name (get (vec data)
+                  (inc (get (indexes-of data) pair)))]
+    (assoc flex
+           :data (map (fn [[pos n]] (if (< id n)
+                                      [(- pos (inc (count name))) n]
+                                      [pos n]))
+                      (remove (fn [[pos x]] (= id x)) data))
+           :index (str (subs index 0 (dec (first pair)))
+                       (subs index (+ (first pair) (count name)))))))
 
 
-
+;;pense 2 estrategias...o hacemos remove-add con lo cual cambiamos la posicion del id de su lugar al final
+;;o modificamos el str agregando o sacando caracteres segun corresponda y sumando la diferencia a las posiciones
+(defn flex-update [flex id value]
+  (let [flex (flex-remove flex id)]
+    (flex-add flex [[id value]])))
 
 (defn find-positions [text search]
   (let [search-len (count search)]
     (loop [from 0
            r []]
       (if-let [i (string/index-of text search from)]
-        (recur (+ (int i) search-len) (conj r i))
+        (recur (+ (int i) search-len)
+               (conj r i))
         r))))
 
 (defn flex-search [{:keys [index data tokenizer filter encoder]} search]
@@ -124,7 +159,9 @@
       (apply sets/intersection
              (mapv #(set (mapv (fn [i]
                                  (last (first (pss/rslice data [(inc i) nil] [-1 nil]))))
-                               (find-positions index %))) words)))))
+                               (find-positions index %)))
+                   words)))))
+
 
 (defn -main [& args]
   (let [sample-data (read-string (slurp "data.edn"))
@@ -135,29 +172,3 @@
     (read-line)
     (mapv sample-data (time (flex-search flex "gal lade")))
     (read-line)))
-
-
-(def pares (map vector (range) (read-string (slurp "data.edn"))))
-
-(time (flex-add (init {:encoder :simple})
-                (map vector (range) (read-string (slurp "data.edn")))))
-
-
-(map vector (range) ["$ aka Dollars"
-                     "$1,000 a Touchdown"
-                     "$10 Raise"
-                     "$10,000 Under a Pillow"
-                     "$50,000 Reward"]))
-
-'([0 "$ aka Dollars"]
-  [1 "$1,000 a Touchdown"]
-  [2 "$10 Raise"]
-  [3 "$10,000 Under a Pillow"]
-  [4 "$50,000 Reward"]
-  [5 "(500) Days of Summer"])
-
-{:encoder
- :tokenizer
- :filter
- :data #{[0 0] [14 1] [33 2] [43 3] [66 4]}
- :index "$ aka dollars$$1,000 a touchdown$$10 raise$"}
